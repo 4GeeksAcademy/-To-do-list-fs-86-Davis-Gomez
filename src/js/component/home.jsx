f975a7c28c145f06722d7cac1331963d45993df9
Davidgmzc <davidgomz.c13@gmail.com>
2024-12-16 00:08:07 +0000

    Finished without css

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React,{ useState } from 'react'
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const lista = []
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+function App() {
 
-export default Home;
+  const [state, setState] = useState(lista)
+  const [color, setColor] = useState("")
+
+
+  const agregar = () => {
+    setState([...state, {color: color}])
+    setColor("")
+  }
+
+  const eliminarItem = (listItemIndex) => {
+    const list = state.filter((listItem, index) => {
+      return index != listItemIndex
+    })
+    setState(list)
+  }
+
+  return (
+    <>
+      <h1>To Do List</h1>
+      <br/>
+        <input value={color}  onChange={(e) => {
+          setColor(e.target.value)
+        }}></input>
+        <br/>
+        <button onClick={agregar}>Agregar</button>
+        <br/>
+        <br/>
+      <ul>
+        {state.map((item, index) => {
+          return (<>
+            <li>
+              {item.color} <button className='btn ' onClick={() => eliminarItem(index)}>x</button>
+            </li>
+          </>)
+        })}
+      </ul>
+      <p>{state.length} Items to do</p>
+    </>
+  )
+}
+
+export default App;
